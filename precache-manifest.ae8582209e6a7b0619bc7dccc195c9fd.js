81521331bdb8598c2b9541ec917b1ad8fc532e70
gloer <gloer@langslet.com>
2019-11-12 14:17:48 +0100

    Updates

---
+++ precache-manifest.ae8582209e6a7b0619bc7dccc195c9fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b919f5ab0af2f6aed32680b3d279f78a",
+    "url": "/box-map-react/index.html"
+  },
+  {
+    "revision": "68794244d25204cdb2fa",
+    "url": "/box-map-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8ce969cffb32ce3474d9",
+    "url": "/box-map-react/static/js/2.c2d52b07.chunk.js"
+  },
+  {
+    "revision": "68794244d25204cdb2fa",
+    "url": "/box-map-react/static/js/main.1edcf935.chunk.js"
+  },
+  {
+    "revision": "a27b9bf0b4e7f53efc66",
+    "url": "/box-map-react/static/js/runtime-main.b6ca619d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/box-map-react/static/media/logo.25bf045c.svg"
+  }
+]);
